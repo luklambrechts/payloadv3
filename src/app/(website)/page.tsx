1845d999aa706012b71bc7bf1cb805e632ce5c9c
Luk Lambrechts <luk@lenoweb.be>
2024-08-12 16:02:22 +0200

    t40:02

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
-export default function Page() {
-  return <div className="p-4 bg-green-100 text-xl text-green-500">Hello world !</div>
-}
+import PageTemplate from './[slug]/page'
+
+export default PageTemplate
---
+++ src/blocks/image/Server.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Server({ page }) {
+  return <div></div>
+}
