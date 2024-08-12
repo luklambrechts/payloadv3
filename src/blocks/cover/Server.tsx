22e7a64ee77bf0bfce6e824ece03bf9725fbbb0e
Luk Lambrechts <luk@lenoweb.be>
2024-08-12 15:55:56 +0200

    t39:02 coverblock version 2 created

--- src/blocks/cover/Server.tsx
+++ src/blocks/cover/Server.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CoverBlockServer({ title, subtitle }) {
   return (
-    <div className="max-w-5xl py-20">
+    <div className="max-w-5xl py-20 text-center">
       <h1 className="text-3xl font-bold">{title}</h1>
       <p className="text-xl">{subtitle}</p>
     </div>
